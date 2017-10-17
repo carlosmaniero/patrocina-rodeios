b3565c8365d2899490b780624ab56ab40ea9c644
Carlos Maniero <carlosmaniero@gmail.com>
2017-10-17 20:34:13 -0200

    Update search tests to use the home page messages

--- tests/Search/SearchSpec.elm
+++ tests/Search/SearchSpec.elm
@@ -4,6 +4,8 @@ import Search.View exposing (search)
 import Search.Update as Update
 import Search.Msg as Msg
 import Msgs
+import Page.Home.Msgs
+import Search.Msg
 import Html.Attributes as Attributes
 import Test exposing (..)
 import Test.Html.Query as Query
@@ -51,21 +53,47 @@ tests =
                     Query.has [ Selector.attribute <| Attributes.value "Cruel Company" ] <|
                         Query.find [ Selector.tag "input" ] <|
                             Query.fromHtml <|
-                                search <|
+                                search
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Input
+                                    )
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Focus
+                                    )
                                     { term = "Cruel Company", result = [], label = "", userSearching = True }
             , test "that search throws the search input event when user types" <|
                 \() ->
-                    Event.expect (Msgs.Search <| Msg.Input "a") <|
-                        Event.simulate (Event.input "a") <|
+                    Event.expect (Msgs.PageHome <| Page.Home.Msgs.Search <| Msg.Input "text-typed") <|
+                        Event.simulate (Event.input "text-typed") <|
                             Query.find [ Selector.tag "input" ] <|
                                 Query.fromHtml <|
-                                    search { term = "", result = [], label = "", userSearching = True }
+                                    search
+                                        (Msgs.PageHome
+                                            << Page.Home.Msgs.Search
+                                            << Search.Msg.Input
+                                        )
+                                        (Msgs.PageHome
+                                            << Page.Home.Msgs.Search
+                                            << Search.Msg.Focus
+                                        )
+                                        { term = "", result = [], label = "", userSearching = True }
             , test "that search renders the label" <|
                 \() ->
                     Query.has [ Selector.text "Find Companies" ] <|
                         Query.find [ Selector.tag "h2" ] <|
                             Query.fromHtml <|
-                                search { term = "", result = [], label = "Find Companies", userSearching = True }
+                                search
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Input
+                                    )
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Focus
+                                    )
+                                    { term = "", result = [], label = "Find Companies", userSearching = True }
             , test "that search renders quick view given a result" <|
                 \() ->
                     let
@@ -73,7 +101,16 @@ tests =
                             Query.findAll [ Selector.tag "li" ] <|
                                 Query.find [ Selector.class "search-quick-view" ] <|
                                     Query.fromHtml <|
-                                        search { term = "", result = validCompanies, label = "Find Companies", userSearching = True }
+                                        search
+                                            (Msgs.PageHome
+                                                << Page.Home.Msgs.Search
+                                                << Search.Msg.Input
+                                            )
+                                            (Msgs.PageHome
+                                                << Page.Home.Msgs.Search
+                                                << Search.Msg.Focus
+                                            )
+                                            { term = "", result = validCompanies, label = "Find Companies", userSearching = True }
                     in
                         Expect.all
                             [ Query.has
@@ -103,25 +140,61 @@ tests =
                     Query.has [ Selector.class "hidden" ] <|
                         Query.find [ Selector.class "search-quick-view" ] <|
                             Query.fromHtml <|
-                                search { term = "", result = [], label = "Find Companies", userSearching = True }
+                                search
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Input
+                                    )
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Focus
+                                    )
+                                    { term = "", result = [], label = "Find Companies", userSearching = True }
             , test "that search quick view is hidden when user is not searching" <|
                 \() ->
                     Query.has [ Selector.class "hidden" ] <|
                         Query.find [ Selector.class "search-quick-view" ] <|
                             Query.fromHtml <|
-                                search { term = "", result = validCompanies, label = "Find Companies", userSearching = False }
+                                search
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Input
+                                    )
+                                    (Msgs.PageHome
+                                        << Page.Home.Msgs.Search
+                                        << Search.Msg.Focus
+                                    )
+                                    { term = "", result = validCompanies, label = "Find Companies", userSearching = False }
             , test "that search throws the search focus true event when user enters the input field" <|
                 \() ->
-                    Event.expect (Msgs.Search <| Msg.Focus True) <|
+                    Event.expect (Msgs.PageHome <| Page.Home.Msgs.Search <| Msg.Focus True) <|
                         Event.simulate (Event.focus) <|
                             Query.find [ Selector.tag "input" ] <|
                                 Query.fromHtml <|
-                                    search { term = "", result = [], label = "", userSearching = False }
+                                    search
+                                        (Msgs.PageHome
+                                            << Page.Home.Msgs.Search
+                                            << Search.Msg.Input
+                                        )
+                                        (Msgs.PageHome
+                                            << Page.Home.Msgs.Search
+                                            << Search.Msg.Focus
+                                        )
+                                        { term = "", result = [], label = "", userSearching = False }
             , test "that search throws the search focus false event when user leaves the input field" <|
                 \() ->
-                    Event.expect (Msgs.Search <| Msg.Focus False) <|
+                    Event.expect (Msgs.PageHome <| Page.Home.Msgs.Search <| Msg.Focus False) <|
                         Event.simulate (Event.blur) <|
                             Query.find [ Selector.tag "input" ] <|
                                 Query.fromHtml <|
-                                    search { term = "", result = [], label = "", userSearching = True }
+                                    search
+                                        (Msgs.PageHome
+                                            << Page.Home.Msgs.Search
+                                            << Search.Msg.Input
+                                        )
+                                        (Msgs.PageHome
+                                            << Page.Home.Msgs.Search
+                                            << Search.Msg.Focus
+                                        )
+                                        { term = "", result = [], label = "", userSearching = True }
             ]
