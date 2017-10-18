916271f4e2ed7466a6a9595992e9bbdf9d9d5a29
Carlos Maniero <carlosmaniero@gmail.com>
2017-10-18 00:22:15 -0200

    Add tests of the page company detail

---
+++ tests/Page/CompanyDetail/UpdateSpec.elm
@@ -0,0 +1,30 @@
+module Page.CompanyDetail.UpdateSpec exposing (..)
+
+import Page.CompanyDetail.Update
+import Page.CompanyDetail.Msgs
+import Page.CompanyDetail.Models
+import Test exposing (..)
+import Expect
+
+
+tests : Test
+tests =
+    describe "CompanyDetail Page Update"
+        [ test "that given a valid company response the model is updated" <|
+            \() ->
+                let
+                    company =
+                        { name = "Cruel Company"
+                        , link = "http://cruel-company.com"
+                        , twitter = "http://twitter.com/cruel-company"
+                        , image = "http://cruel-company.com/logo.png"
+                        , slug = "cruelcompany"
+                        }
+
+                    ( updatedModel, cmd ) =
+                        Page.CompanyDetail.Update.update
+                            (Page.CompanyDetail.Msgs.CompanyDataLoaded <| Ok company)
+                            Page.CompanyDetail.Models.init
+                in
+                    Expect.equal (Just company) updatedModel.company
+        ]
