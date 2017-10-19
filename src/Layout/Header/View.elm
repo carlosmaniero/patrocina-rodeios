7d60fefaeabeccbddd928f4cc9fb76a4b8699536
Carlos Maniero <carlosmaniero@gmail.com>
2017-10-19 03:33:11 -0200

    Add link to home in header

--- src/Layout/Header/View.elm
+++ src/Layout/Header/View.elm
@@ -3,10 +3,14 @@ module Layout.Header.View exposing (mainHeader)
 import Layout.Header.Model exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Router.Link
+import Router.Model
+import Msgs
 
 
-mainHeader : Model -> Html msg
+mainHeader : Model -> Html Msgs.Msg
 mainHeader model =
     header [ class "main-header" ]
-        [ h1 [] [ text model.title ]
+        [ h1 []
+            [ Router.Link.renderLink Router.Model.Home [] [ text model.title ] ]
         ]
